c084a99ce8e0c4c9dba10d833b15293b9c8293a0
jmvvalle <educational+dugong-t24d2@norton.me>
2025-02-23 21:52:54 -0600

    Cargar credenciales desde variable de entorno

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
+import os
 
-# Cargar credenciales desde el archivo JSON que subiste
-cred = credentials.Certificate("simulacion-economia.json")  
+# Cargar credenciales desde la variable de entorno
+firebase_key = os.getenv("FIREBASE_KEY")
+
+if not firebase_key:
+    raise ValueError("No se encontró la variable de entorno FIREBASE_KEY")
+
+cred_dict = json.loads(firebase_key)
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 
 # Conectar con Firestore
